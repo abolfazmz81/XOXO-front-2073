a7171e6a8e02b029aad9be677c6ca6737530f7de
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-06 00:39:15 +0330

    feat: Add routes and router to App.jsx

---
+++ src/components/Game Design/GameDesign.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const gameDesign = () =>{
+    return(
+        <div></div>
+    );
+};
+
+export default gameDesign;
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const homePage = () =>{
+    return(
+        <div> salam </div>
+    );
+};
+
+export default homePage;
