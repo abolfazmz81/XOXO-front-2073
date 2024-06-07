3e7d4a42f389a4781b211427ad27b0e024356793
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-07 19:10:46 +0330

    feat: Implement Header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './Header.css'
 const Header = () =>{
     return(
-        <div className=""> 
-            bruh
+        <div className="header h2"> 
+            Tic-Tac-Toe
         </div>
-        
     );
 };
 
