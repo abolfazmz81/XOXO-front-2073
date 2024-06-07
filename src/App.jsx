809357f0925faf70b19bc561b5c2559fbadb6715
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-07 17:49:34 +0330

    feat: Add bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import { useState } from 'react';
 import './App.css';
 import {BrowserRouter,Route,Router,Routes} from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
-import Login from './components/Login/Login'
-import Table from './components/Table/Table'
+import Login from './components/Login/Login';
+import Table from './components/Table/Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (<>
