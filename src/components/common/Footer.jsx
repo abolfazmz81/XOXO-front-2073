b5e1435973f81fe6c311fb428ffda220dec6ee35
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-07 20:25:49 +0330

    feat: Implemet footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import './footer.css'
 const Footer = () =>{
     return(
-        <div className="" > 
-            
+        <div className="footer h4" > 
+            Proved by Abolfazl Mohammadi 2024
         </div>
     );
 };
