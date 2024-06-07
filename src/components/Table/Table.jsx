442574020ed0788aceec3d1404426a3c7b0a5514
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-07 22:27:19 +0330

    feat: Add fetch to get top 10

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,9 +1,23 @@
+import React, { useEffect, useState } from "react";
+import Header from "../common/Header";
+
+const Table = () => {
+  const [Tops, setTops] = useState([]);
+  useEffect(() => {
+    async function LoadTops() {
+      const response = await fetch("http://127.0.0.1:8000/top", {
+        method: "GET",
+      }).then((res) => res.json().then((data) => setTops(data)));
+    }
+    LoadTops();
+  }, []);
+
 import React from "react";
 
 const Table = () =>{
     return(
         <div > bye </div>
-    );
+  );
 };
 
 export default Table;
