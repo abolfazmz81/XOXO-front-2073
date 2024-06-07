fae4b9512191d8cf5974274a3add44ae1fe80b7f
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-07 18:42:13 +0330

    feat: Add common components

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Header = () =>{
+    return(
+        <div className=""> 
+            bruh
+        </div>
+        
+    );
+};
+
+export default Header;
