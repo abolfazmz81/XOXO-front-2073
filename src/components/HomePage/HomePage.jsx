7cd694b65a51cbee032f70b0a7160fe726a3a112
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-07 20:01:28 +0330

    feat: Add Start and Scores

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,15 +3,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './HomePage.css';
 import Header from "../common/Header";
 import Footer from "../common/Footer";
+import { Navigate, useNavigate } from "react-router-dom";
 
 
 
 const HomePage = () =>{
+    const navigate = useNavigate();
+
+    const handleStartClick = () => {
+        navigate('/login'); 
+    };
+    const handleScoresClick = () => {
+        navigate('/scores'); 
+    };
     return(
         <div className="All d-flex  flex-column align-items-center justify-content-around" > 
             <Header/>
-            <div className=""> 
-        
+            <div className="d-flex flex-column align-items-center"> 
+                <div className="start btn btn-light btn-lg" onClick={handleStartClick}> START</div>
+                <div className="scores btn mt-4" onClick={handleScoresClick} > SCORES </div>
             </div>
             <Footer/>
         </div>
