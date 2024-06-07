1bf9416243f9604ad2737a718ec1355560723428
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-07 22:28:27 +0330

    feat: Add row generator(map)

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from "react";
 import Header from "../common/Header";
 
 const Table = () => {
+  let count = 0;
+  const getcount = () => {
+    count += 1;
+    return count;
+  };
   const [Tops, setTops] = useState([]);
   useEffect(() => {
     async function LoadTops() {
@@ -12,11 +17,25 @@ const Table = () => {
     LoadTops();
   }, []);
 
-import React from "react";
-
-const Table = () =>{
-    return(
-        <div > bye </div>
+  return (
+    <div className="d-flex flex-column All align-items-center justify-content-around">
+      <Header />
+      <div className="header h1">SCORE TABLE TOP(10)</div>
+      <div className="container">
+        <div className="row text-center fw-bold py-2">
+          <div className="col header">ROW</div>
+          <div className="col header">NAME</div>
+          <div className="col header">SCORE</div>
+        </div>
+        {Tops.map((user) => (
+          <div className="row text-center py-2" key={user.name}>
+            <div className="col header">{getcount()}</div>
+            <div className="col header">{user.name}</div>
+            <div className="col header">{user.score}</div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
