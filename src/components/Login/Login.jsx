f42ecae029e8e88fa435832a1edcc788c9802ff5
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-08 19:22:51 +0330

    feat: Add navigate conditions

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,6 +4,24 @@ import Footer from "../common/Footer";
 import { useNavigate } from "react-router-dom";
 import "./login.css";
 const Login = () =>{
+    const [playerOne, setPlayerOne] = useState("");
+    const [playerTwo, setPlayerTwo] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(playerOne == " " || playerTwo == " "){
+            alert("one player name is space")
+            return;
+        }
+        if(playerOne == playerTwo){
+            alert("cant have two players with the same name")
+            return;
+        }
+        // Pass player names to another component or navigate
+        // Example of navigating to a game component with player names as state
+        navigate("/play", { state: { playerOne, playerTwo } });
+      };
     return(
         <div> bia </div>
     );
