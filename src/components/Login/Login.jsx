eae9e236274b37b6c0be14b083d296f5e9d9d727
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-08 19:20:32 +0330

    feat: Add needed functions

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,8 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Header from "../common/Header";
+import Footer from "../common/Footer";
+import { useNavigate } from "react-router-dom";
+import "./login.css";
 const Login = () =>{
     return(
         <div> bia </div>
