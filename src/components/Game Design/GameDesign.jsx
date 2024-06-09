0c5aacf2ac615ef6c5ba910d7f56237c52dbec1b
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-09 19:34:33 +0330

    feat: Add Play page structure

--- src/components/Game Design/GameDesign.jsx
+++ src/components/Game Design/GameDesign.jsx
@@ -24,7 +24,29 @@ const GameDesign = () =>{
     };
 
     return(
-        <div></div>
+        <div className="All d-flex  flex-column align-items-center justify-content-evenly">
+            <Header/>
+            <div className="container-xxl d-flex flex-column text-center flex-4 justify-content-between">
+                <div className="d-flex  justify-content-between">
+                    <h4 className="text-white">X is: {playerOne}</h4>
+                    <h4 className="text-white">O is: {playerTwo}</h4>
+                </div>
+                <div className="board container d-flex flex-column justify-content-center align-items-center">
+                    <div className="row w-100">
+                        {board.map((cell, index) => (
+                            <div 
+                                key={index}
+                                className="col-4 cell d-flex justify-content-center align-items-center"
+                                onClick={() => handleClick(index)}
+                            >
+                                {cell === "X" && <span className="text-red">{cell}</span>}
+                                {cell === "O" && <span className="text-blue">{cell}</span>}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
